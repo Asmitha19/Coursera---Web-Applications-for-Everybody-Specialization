a7040e971c7eea883382fd2d9298641f092a271a
Asmitha19 <67317332+Asmitha19@users.noreply.github.com>
2020-08-11 11:05:38 +0530

    Add files via upload

---
+++ Building Web Applications in PHP/week 5/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Asmitha PHP</title>
+  </head>
+  <body>
+    <h1>Asmitha PHP</h1>
+    <p>
+      <?php
+        print "The SHA256 hash of \"Asmitha\" is " . hash('sha256', 'Asmitha');
+      ?>
+    </p>
+    <pre>
+            **
+           ****
+          **  **
+         **    **
+        **********
+       **        **
+      **          **
+     **            **
+    **              **
+    </pre>
+    <a href="check.php">Click here to check the error setting</a><br/>
+    <a href="fail.php">Click here to cause a traceback</a>
+  </body>
+</html>
