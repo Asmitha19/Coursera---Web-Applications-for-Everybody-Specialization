bf4d2aab7c4023db4c98f1526a3e4b4a478da10d
Asmitha19 <67317332+Asmitha19@users.noreply.github.com>
2020-08-11 11:31:40 +0530

    Add files via upload

---
+++ Building Database Applications in PHP/week 2/autos.php
@@ -0,0 +1,77 @@
+<?php
+require_once "pdo.php";
+
+$failure = false;
+$success = false;
+
+if (!isset($_GET['name'])) {
+    die("Name parameter missing");
+} elseif (isset($_POST['logout']) && $_POST['logout'] == 'Logout') {
+    header('Location: index.php');
+} elseif (isset($_POST['make']) && isset($_POST['year']) && isset($_POST['mileage'])) {
+    if (!is_numeric($_POST['year']) || !is_numeric($_POST['mileage'])) {
+        $failure = 'Mileage and year must be numeric';
+    } elseif (strlen($_POST['make']) < 1 ) {
+        $failure = 'Make is required';
+    } else {
+      $stmt = $pdo->prepare('INSERT INTO autos (make, year, mileage) VALUES ( :mk, :yr, :mi)');
+      $stmt->execute(array(
+              ':mk' => htmlentities($_POST['make']),
+              ':yr' => htmlentities($_POST['year']),
+              ':mi' => htmlentities($_POST['mileage']))
+      );
+      $success = 'Record inserted';
+    }
+}
+$stmt = $pdo->query("SELECT make, year, mileage FROM autos");
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once "bootstrap.php"; ?>
+<title>Asmitha's (6a76035c) Autos Page</title>
+</head>
+<body>
+<div class="container">
+<h1>Tracking Autos for <?php echo $_GET['name']; ?></h1>
+<?php
+if ($failure !== false) {
+    // Look closely at the use of single and double quotes
+    echo('<p style="color: red;">' . htmlentities($failure) . "</p>\n");
+}
+if ($success !== true) {
+    // Look closely at the use of single and double quotes
+    echo('<p style="color: green;">' . htmlentities($success) . "</p>\n");
+}
+?>
+
+<form method="post">
+<p>Make:
+<input type="text" name="make"/></p>
+<p>Year:
+<input type="text" name="year"/></p>
+<p>Mileage:
+<input type="text" name="mileage"/></p>
+<input type="submit" value="Add">
+<input type="submit" name="logout" value="Logout">
+</form>
+<br><br>
+<table style="width:30%; text-align:center; border: 2px solid black" border="1">
+<caption style="text-align:center">Automobiles</caption>
+<?php
+foreach ( $rows as $row ) {
+    echo "<tr><td>";
+    echo($row['make']);
+    echo("</td><td>");
+    echo($row['year']);
+    echo("</td><td>");
+    echo($row['mileage']);
+    echo("</td></tr>\n");
+}
+?>
+</table>
+</div>
+</body>
---
+++ Building Database Applications in PHP/week 2/index.php
@@ -0,0 +1,25 @@
+<!-- Assignment: Automobiles and Databases
+     https://www.wa4e.com/solutions/autosdb/login.php-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Asmitha (6a76035c) - Autos Database</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Autos Database</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+</p>
+<p>
+<a href="https://www.wa4e.com/assn/autosdb/" target="_blank">Specification for this Application</a>
+</p>
+</div>
+</body>
