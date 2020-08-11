346858eead4efb6c015c35f5e0488098b212a370
Asmitha19 <67317332+Asmitha19@users.noreply.github.com>
2020-08-11 11:12:29 +0530

    Add files via upload

---
+++ Building Web Applications in PHP/week 8/index.php
@@ -0,0 +1,25 @@
+<!-- Assignment: Login and Rock Paper Scissors
+     https://www.wa4e.com/assn/rps/-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>ASMITHA B af7e75b8 - Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
+</body>
