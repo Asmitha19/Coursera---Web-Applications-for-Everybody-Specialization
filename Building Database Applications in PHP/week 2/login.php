5244122ec2a0551c0679989bc3065d57b6c8a68b
Asmitha19 <67317332+Asmitha19@users.noreply.github.com>
2020-08-11 11:34:11 +0530

    Update login.php

--- Building Database Applications in PHP/week 2/login.php
+++ Building Database Applications in PHP/week 2/login.php
@@ -40,7 +40,7 @@ if (isset($_POST['who']) && isset($_POST['pass'])) {
 <html>
 <head>
 <?php require_once "bootstrap.php"; ?>
-<title>Asmitha's (6a76035c) Login Page</title>
+<title>Asmitha's Login Page</title>
 </head>
 <body>
 <div class="container">
